f27891d7b15419a051dff66fb89f0c6b31e61595
tajbaba999 <tajbaba153@gmail.com>
2025-07-28 23:24:58 +0530

    fix: fix some issue in dashboard

--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -13,7 +13,8 @@ import {
 
 export function SectionCards() {
   return (
-    <div className="p-4 flex gap-5">
+    // <div className="p-4 overflow-x-auto">
+      <div className="p-4 flex gap-5 ">
       <div className="transition-transform duration-300 hover:scale-105">
       <Card>
         <CardHeader>
@@ -70,8 +71,22 @@ export function SectionCards() {
         </CardFooter>
       </Card>
       </div>
-      
-    </div>
+       <div className="transition-transform duration-300 hover:scale-105">
+      <Card>
+        <CardHeader>
+          <CardTitle>Sample Card</CardTitle>
+          <CardDescription>This is a sample description.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>This is the content of the card.</p>
+        </CardContent>
+        <CardFooter>
+          <p>Footer info</p>
+        </CardFooter>
+      </Card>
+      </div>  
+      </div>
+    // </div>
   )
 }
 
